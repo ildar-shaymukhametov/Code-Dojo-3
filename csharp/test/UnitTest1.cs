c6b8c4e82a10a23b7a16ea8993045557b0e1d453
Шаймухаметов Ильдар <shaimildar@gmail.com>
2021-08-08 18:00:28 +0500

    rename variable

--- csharp/test/UnitTest1.cs
+++ csharp/test/UnitTest1.cs
@@ -10,13 +10,13 @@ namespace test
         public void MinTempSpreadDayCalculator___GetDay___Calculates_day_of_min_temp_spread()
         {
             var sut = new MinTempSpreadDayCalculator(new MinSpreadIndexCalculator());
-            var content = @"  Dy MxT   MnT   AvT   HDDay  AvDP 1HrP TPcpn WxType PDir AvSp Dir MxS SkyC MxR MnR AvSLP
+            var data = @"  Dy MxT   MnT   AvT   HDDay  AvDP 1HrP TPcpn WxType PDir AvSp Dir MxS SkyC MxR MnR AvSLP
 
    1  10    5    74          53.8       0.00 F       280  9.6 270  17  1.6  93 23 1004.5
    2  8    1    71          46.5       0.00         330  8.7 340  23  3.3  70 28 1004.5
    3  12    10    66          39.6       0.00         350  5.0 350   9  2.8  59 24 1016.8
   mo  82.9  60.5  71.7    16  58.8       0.00              6.9          5.3";
-            var actual = sut.GetDay(content);
+            var actual = sut.GetDay(data);
             Assert.Equal(3, actual);
         }
 
@@ -24,7 +24,7 @@ namespace test
         public void MinGoalSpreadTeamCalculator___GetTeam___Returns_team_with_min_goal_spread()
         {
             var sut = new MinGoalSpreadTeamCalculator(new MinSpreadIndexCalculator());
-            var content = @"       Team            P     W    L   D    F      A     Pts
+            var data = @"       Team            P     W    L   D    F      A     Pts
     1. Arsenal         38    26   9   3    8  -  4    87
     2. Liverpool       38    24   8   6    17  -  3    80
     3. Manchester_U    38    24   5   9    11  -  5    77
@@ -33,7 +33,7 @@ namespace test
    19. Derby           38     8   6  24    13  -  6    30
    20. Leicester       38     5  13  20    33  -  6    28
 ";
-            var actual = sut.GetTeam(content);
+            var actual = sut.GetTeam(data);
             Assert.Equal("Ipswich", actual);
         }
 
