b9c59a7bd31debfc95ce09336011ae9505c6e19a
Шаймухаметов Ильдар <shaimildar@gmail.com>
2021-08-08 11:59:03 +0500

    first program

--- csharp/src/Program.cs
+++ csharp/src/Program.cs
@@ -1,5 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
+using System.IO;
+using System.Linq;
 
 namespace src
 {
@@ -7,7 +8,39 @@ namespace src
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        }
+    }
+    
+    public class MinTempSpreadCalculator
+    {
+        public int GetDay(string fileContents)
+        {
+            var result = fileContents
+                .Split("\r\n")
+                .Skip(2)
+                .SkipLast(1)
+                .Select(x => x
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray())
+                .Select(x => new
+                {
+                    Day = int.Parse(x[0]),
+                    MaxTemp = int.Parse(new string(x[1].Where(Char.IsDigit).ToArray())),
+                    MinTemp = int.Parse(new string(x[2].Where(Char.IsDigit).ToArray()))
+                })
+                .Aggregate((acc, v) =>
+                {
+                    var prevSpread = acc.MaxTemp - acc.MinTemp;
+                    var currSpread = v.MaxTemp - v.MinTemp;
+                    if (prevSpread < currSpread)
+                    {
+                        return acc;
+                    }
+                    return v;
+                })
+                .Day;
+
+            return result;
         }
     }
 }
--- csharp/test/UnitTest1.cs
+++ csharp/test/UnitTest1.cs
@@ -6,8 +6,17 @@ namespace test
     public class UnitTest1
     {
         [Fact]
-        public void Test()
+        public void MinTempSpreadCalculator___GetDay___Calculates_day_of_min_temp_spread()
         {
+            var sut = new MinTempSpreadCalculator();
+            var content = @"  Dy MxT   MnT   AvT   HDDay  AvDP 1HrP TPcpn WxType PDir AvSp Dir MxS SkyC MxR MnR AvSLP
+
+   1  10    5    74          53.8       0.00 F       280  9.6 270  17  1.6  93 23 1004.5
+   2  8    1    71          46.5       0.00         330  8.7 340  23  3.3  70 28 1004.5
+   3  12    10    66          39.6       0.00         350  5.0 350   9  2.8  59 24 1016.8
+  mo  82.9  60.5  71.7    16  58.8       0.00              6.9          5.3";
+            var actual = sut.GetDay(content);
+            Assert.Equal(3, actual);
         }
     }
 }
